36265851d6975e546af57ae26604f4eaba488e91
Nicolas Schuck <nico-schuck@online.de>
2022-01-17 11:05:29 +0100

    modified

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install git
 sudo apt-get install libcurl4
 sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
 sudo add-apt-repository ppa:linuxuprising/libpng12
-sud apt update
+sudo apt update
 sudo apt install libpng12-0
 sudo apt-get install libncurses5-dev libncursesw5-dev
 sudo apt-get install python
