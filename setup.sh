ba476131048bd5438e05ea8514981f667600609e
Nicolas Schuck <nico-schuck@online.de>
2022-01-17 11:03:20 +0100

    modified

--- setup.sh
+++ setup.sh
@@ -12,3 +12,8 @@ sudo apt install libpng12-0
 sudo apt-get install libncurses5-dev libncursesw5-dev
 sudo apt-get install python
 sudo apt-get install python3
+sudo apt-get install gawk wget git-core diffstat unzip texinfo \
+gcc-multilib  build-essential chrpath socat cpio python python3 \
+python3-pip python3-pexpect  xz-utils debianutils iputils-ping \
+python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm \
+libncurses-dev gawk flex bison openssl libssl-dev
